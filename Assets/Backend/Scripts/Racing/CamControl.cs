9ebbf3b78134d4eb7f40b800470a70c72a851a6f
Aditya Iyer <adityaiyer2007@gmail.com>
2026-07-10 19:14:19 -0700

    Camera switching script

--- Assets/Backend/Scripts/Racing/CamControl.cs
+++ Assets/Backend/Scripts/Racing/CamControl.cs
@@ -1,16 +1,36 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CamControl : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public List<GameObject> cameraParents;
+    private int currentCamIndex = 0;
+
     void Start()
     {
+        Camera[] allCameras =
+            FindObjectsByType<Camera>(FindObjectsInactive.Include, FindObjectsSortMode.None);
         
+        foreach (Camera camera in allCameras)
+        {
+            GameObject cameraParent = camera.transform.parent.gameObject;
+            if (cameraParent.tag != "CamParent") continue;
+            cameraParent.SetActive(false);
+            cameraParents.Add(cameraParent);
+        }
+        cameraParents[currentCamIndex].SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.cKey.wasPressedThisFrame)
+        {
+            cameraParents[currentCamIndex].SetActive(false);
+            currentCamIndex = (currentCamIndex+1) % cameraParents.Count;
+            cameraParents[currentCamIndex].SetActive(true);
+        }
     }
 }
