f6ae594e4ca274efe79eff3b7001d5286755db78
Aditya Iyer <adityaiyer2007@gmail.com>
2026-07-17 23:29:21 -0700

    Starting ECS implementation (I hate this)

---
+++ Assets/Backend/Scripts/Management/RaceManagerAuthoring.cs
@@ -0,0 +1,167 @@
+using UnityEngine;
+using Unity.Entities;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
+public class RaceConfig : IComponentData
+{
+    public CarProfile carProfile = default;
+    public GroundSpeedModifier asphaltModifier = default;
+    public GroundSpeedModifier gravelModifier = default;
+}
+
+public struct RaceState : IComponentData
+{
+    
+}
+
+public class RaceManagerAuthoring : MonoBehaviour
+{
+    public static RaceManagerAuthoring Instance { get; private set; }
+
+
+    class Baker : Baker<RaceManagerAuthoring>
+    {
+        public override void Bake(RaceManagerAuthoring authoring)
+        {
+            
+        }
+    }
+
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    
+    public bool isOnGround { get; private set; }
+    public bool isOnTrack { get; private set; }
+
+    public bool useSmartSteer;
+
+    [Header("Car & Track Qualities")]
+    public CarProfile carProfile = default;
+    public GroundSpeedModifier asphaltModifier = default;
+    public GroundSpeedModifier gravelModifier = default;
+
+    public GroundSpeedModifier currentModifier {
+        get => isOnTrack ? asphaltModifier : gravelModifier;
+    }
+
+    public float bestLapTime {get; private set;} = float.NaN;
+    public List<float> lapTimes {get; private set;} = new();
+
+    [HideInInspector] public List<TurnTrackerScript> checkpoints
+        {get; private set; } = new();
+    [HideInInspector] public List<TurnTrackerScript> checkpointsCrossed
+        {get; private set; } = new();
+    public float lastTimeToPassStart {get; private set; } = float.NaN;
+    public Transform playerCar { get; private set; }
+    private Vector3 playerInitialPos;
+    private Quaternion playerInitialRot;
+    public bool startComplete { get; private set; } = false;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerCar = GameObject.FindGameObjectWithTag("Player").transform;
+        playerCar.GetComponent<BoxCollider>().size = carProfile.size;
+        playerInitialPos = playerCar.position;
+        playerInitialRot = playerCar.rotation;
+
+        // Find checkpoints
+        Transform checkpointsParent = GameObject.FindGameObjectWithTag("Track")
+            .transform.parent.Find("Checkpoints");
+        for(int i = 0; i < checkpointsParent.childCount; i++)
+        {
+            TurnTrackerScript turn;
+            bool foundComponent =
+                checkpointsParent.GetChild(i).TryGetComponent(out turn);
+            if (foundComponent) checkpoints.Add(turn);
+        }
+        startComplete = true;
+    }
+
+    float CalculateLapTime(float timeToPassFinish)
+    {   // Checks which checkpoints have been crossed and what penalties are due
+        float penalties = 0F;
+        int numCheckpoints = checkpoints.Count;
+        bool[] seenCheckpoints = new bool[numCheckpoints];
+
+        // First mark which all checkpoints are seen
+        foreach (TurnTrackerScript checkpointCrossed in checkpointsCrossed)
+        {
+            int idx = checkpoints.IndexOf(checkpointCrossed);
+            if (idx == -1) continue;
+            seenCheckpoints[idx] = true;
+        }
+
+        // Second tally penalties for unseen checkpoints
+        for (int i=0; i < numCheckpoints; i++)
+        {
+            if (seenCheckpoints[i]) continue;
+            if (checkpoints[i].isDNF) return float.PositiveInfinity;
+            penalties += checkpoints[i].tiemPenalty;
+        }
+
+        return timeToPassFinish - lastTimeToPassStart + penalties;
+    }
+
+    public void FlagPlayerCrossedTurn(TurnTrackerScript turn)
+    {   // Called by TurnTrackerScript (OnTriggerEnter) when player crosses a checkpoint
+        if (!checkpoints.Contains(turn)) return;
+        if (turn.isFinish)
+        {
+            if (!float.IsNaN(lastTimeToPassStart)) {
+                float lapTime = CalculateLapTime(turn.lastCrossedTime);
+                if (!float.IsInfinity(lapTime)) {
+                    lapTimes.Add(lapTime);
+                    if (lapTimes.Count > 5) lapTimes.RemoveAt(0);
+                    if (float.IsNaN(bestLapTime) || lapTime < bestLapTime) bestLapTime = lapTime;
+                }
+            }
+            checkpointsCrossed.Clear();
+        }
+        if (turn.isStart) lastTimeToPassStart = turn.lastCrossedTime;
+        checkpointsCrossed.Add(turn);
+    }
+
+    void Update()
+    {
+        // Reset logic
+        if (Keyboard.current.rKey.wasPressedThisFrame)
+        {
+            Rigidbody rb = playerCar.GetComponent<Rigidbody>();
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            playerCar.position = playerInitialPos;
+            playerCar.rotation = playerInitialRot;
+            checkpointsCrossed.Clear();
+            lastTimeToPassStart = float.NaN;
+        }
+        // Check if car is on ground
+        RaycastHit[] hits = Physics.BoxCastAll(
+            playerCar.position, carProfile.size/2, -playerCar.up,
+            playerCar.rotation, carProfile.size.y*0.1f);
+        isOnGround = false;
+        isOnTrack = false;
+        foreach (RaycastHit hit in hits) {
+            if (hit.transform.tag == "Ground") isOnGround = true;
+            if (hit.transform.tag == "Track")
+            {
+                isOnTrack = true;
+                break;
+            }
+        }
+        isOnGround = isOnGround || isOnTrack;
+    }
+}
